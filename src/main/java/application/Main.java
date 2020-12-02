cd288f3cfebeb38505604ef284b0075aa7147a03
andreas-repo <andreas.mayer@gepardec.com>
2020-12-02 16:01:13 +0100

    -- added comment about the goal

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,7 +1,25 @@
 package application;
 
 public class Main {
+    /*
+    Good morning! Here's your coding interview problem for today.
+
+    This problem was asked by Spotify.
+
+    Write a function, throw_dice(N, faces, total), that determines how many ways it is possible to throw N dice with some number of faces each to get a specific total.
+
+    For example, throw_dice(3, 6, 7) should equal 15.
+     */
     public static void main(String[] args) {
+        throw_dice(3, 6, 7);
+
+
 
     }
+
+    public static String throw_dice(int throwNdice, int faces, int total) {
+        return  null;
+    }
+
+
 }
